31febba1f1f006a41aab428f0aeb12770fb115b3
Neon26 <shaynehakuna26@gmail.com>
2022-09-01 19:59:33 -0500

    fixed errors

--- db/userModel.js
+++ db/userModel.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({    
-    email: { type: String,
-        required: [true, "Please provide an Email!"],
-        unique: [true, "Email already exists!"],
-        },
-    password: { type: String,
-        required: [true, "Please provide a Password!"],
-        unique: false,
-        }
+// user schema
+const UserSchema = new mongoose.Schema({
+  // email field
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+
+  //   password field
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
 });
 
-module.exports = mongoose.model.Users || mongoose.model("Users", userSchema);
+// export UserSchema
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
