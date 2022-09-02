119a5979cf7679b9f183557dd0d46c69b51e227a
Neon26 <shaynehakuna26@gmail.com>
2022-09-01 19:41:42 -0500

    Back End

---
+++ db/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({    
+    email: { type: String,
+        required: [true, "Please provide an Email!"],
+        unique: [true, "Email already exists!"],
+        },
+    password: { type: String,
+        required: [true, "Please provide a Password!"],
+        unique: false,
+        }
+});
+
+module.exports = mongoose.model.Users || mongoose.model("Users", userSchema);
